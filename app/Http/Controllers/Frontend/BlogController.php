43dfafbccb5bfa765c066d494715c0d7e29eae54
echobash-dtdc <anwar.ali@dtdc.com>
2025-07-24 11:50:10 +0530

    Change static template into the dynamic loops

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -8,7 +8,46 @@ class BlogController extends Controller
 {
     public function index()
     {
-        return view('frontend.blogs.index');
+        $blogs = [
+            [
+                'id' => 1,
+                'image' => 'mentor/img/course-1.jpg',
+                'category' => 'Web Development',
+                'price' => '$169',
+                'title' => 'Website Design',
+                'description' => 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
+                'author_image' => 'mentor/img/trainers/trainer-1-2.jpg',
+                'author_name' => 'Antonio',
+                'users_count' => 50,
+                'likes_count' => 65,
+            ],
+            [
+                'id' => 2,
+                'image' => 'mentor/img/course-2.jpg',
+                'category' => 'Marketing',
+                'price' => '$250',
+                'title' => 'Search Engine Optimization',
+                'description' => 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
+                'author_image' => 'mentor/img/trainers/trainer-2-2.jpg',
+                'author_name' => 'Lana',
+                'users_count' => 35,
+                'likes_count' => 42,
+            ],
+            [
+                'id' => 3,
+                'image' => 'mentor/img/course-3.jpg',
+                'category' => 'Content',
+                'price' => '$180',
+                'title' => 'Copywriting',
+                'description' => 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
+                'author_image' => 'mentor/img/trainers/trainer-3-2.jpg',
+                'author_name' => 'Brandon',
+                'users_count' => 20,
+                'likes_count' => 85,
+            ],
+        ];
+
+        return view('frontend.blogs.index', compact('blogs'));
     }
 
     public function show()
--- resources/views/frontend/blogs/index.blade.php
+++ resources/views/frontend/blogs/index.blade.php
@@ -28,87 +28,40 @@
     </div>
     <!-- End Page Title -->
 
-    <!-- Courses Section -->
-    <section id="courses" class="courses section">
+    <!-- Blogs Section -->
+    <section id="blogs" class="courses section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                    <div class="course-item">
-                        <img src="{{ asset('mentor/img/course-1.jpg') }}" class="img-fluid" alt="...">
-                        <div class="course-content">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <p class="category">Web Development</p>
-                                <p class="price">$169</p>
-                            </div>
-                            <h3><a href="{{ route('frontend.blogs.show') }}">Website Design</a></h3>
-                            <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere
-                                quia quae dolores dolorem tempore.</p>
-                            <div class="trainer d-flex justify-content-between align-items-center">
-                                <div class="trainer-profile d-flex align-items-center">
-                                    <img src="{{ asset('mentor/img/trainers/trainer-1-2.jpg') }}" class="img-fluid" alt="">
-                                    <a href="{{ route('frontend.blogs.show') }}" class="trainer-link">Antonio</a>
+                @foreach($blogs as $blog)
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="course-item">
+                            <img src="{{ asset($blog['image']) }}" class="img-fluid" alt="...">
+                            <div class="course-content">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <p class="category">{{ $blog['category'] }}</p>
+                                    <p class="price">{{ $blog['price'] ?? '' }}</p>
                                 </div>
-                                <div class="trainer-rank d-flex align-items-center">
-                                    <i class="bi bi-person user-icon"></i>&nbsp;50
-                                    &nbsp;&nbsp;
-                                    <i class="bi bi-heart heart-icon"></i>&nbsp;65
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
-                    data-aos-delay="200">
-                    <div class="course-item">
-                        <img src="{{ asset('mentor/img/course-2.jpg') }}" class="img-fluid" alt="...">
-                        <div class="course-content">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <p class="category">Marketing</p>
-                                <p class="price">$250</p>
-                            </div>
-                            <h3><a href="{{ route('frontend.blogs.show') }}">Search Engine Optimization</a></h3>
-                            <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere
-                                quia quae dolores dolorem tempore.</p>
-                            <div class="trainer d-flex justify-content-between align-items-center">
-                                <div class="trainer-profile d-flex align-items-center">
-                                    <img src="{{ asset('mentor/img/trainers/trainer-2-2.jpg') }}" class="img-fluid" alt="">
-                                    <a href="{{ route('frontend.blogs.show') }}" class="trainer-link">Lana</a>
-                                </div>
-                                <div class="trainer-rank d-flex align-items-center">
-                                    <i class="bi bi-person user-icon"></i>&nbsp;35
-                                    &nbsp;&nbsp;
-                                    <i class="bi bi-heart heart-icon"></i>&nbsp;42
+                                <h3>
+                                    <a href="{{ route('frontend.blogs.show', $blog['id']) }}">
+                                        {{ $blog['title'] }}
+                                    </a>
+                                </h3>
+                                <p class="description">{{ $blog['description'] }}</p>
+                                <div class="trainer d-flex justify-content-between align-items-center">
+                                    <div class="trainer-profile d-flex align-items-center">
+                                        <img src="{{ asset($blog['author_image']) }}" class="img-fluid" alt="">
+                                        <a href="#" class="trainer-link">{{ $blog['author_name'] }}</a>
+                                    </div>
+                                    <div class="trainer-rank d-flex align-items-center">
+                                        <i class="bi bi-person user-icon"></i>&nbsp;{{ $blog['users_count'] ?? 0 }}
+                                        &nbsp;&nbsp;
+                                        <i class="bi bi-heart heart-icon"></i>&nbsp;{{ $blog['likes_count'] ?? 0 }}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in"
-                    data-aos-delay="300">
-                    <div class="course-item">
-                        <img src="{{ asset('mentor/img/course-3.jpg') }}" class="img-fluid" alt="...">
-                        <div class="course-content">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <p class="category">Content</p>
-                                <p class="price">$180</p>
-                            </div>
-                            <h3><a href="{{ route('frontend.blogs.show') }}">Copywriting</a></h3>
-                            <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere
-                                quia quae dolores dolorem tempore.</p>
-                            <div class="trainer d-flex justify-content-between align-items-center">
-                                <div class="trainer-profile d-flex align-items-center">
-                                    <img src="{{ asset('mentor/img/trainers/trainer-3-2.jpg') }}" class="img-fluid" alt="">
-                                    <a href="{{ route('frontend.blogs.show') }}" class="trainer-link">Brandon</a>
-                                </div>
-                                <div class="trainer-rank d-flex align-items-center">
-                                    <i class="bi bi-person user-icon"></i>&nbsp;20
-                                    &nbsp;&nbsp;
-                                    <i class="bi bi-heart heart-icon"></i>&nbsp;85
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
