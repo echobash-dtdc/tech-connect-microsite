91df51fd3e4dd660c73bcd3f44dfe1ef7095c3ae
echobash-dtdc <anwar.ali@dtdc.com>
2025-07-25 13:31:19 +0530

    Refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,22 @@ use App\Http\Controllers\Frontend\BlogController;
 use App\Http\Controllers\Frontend\FeedBackController;
 use App\Http\Controllers\Frontend\TeamMemberController;
 
-// Home and general pages
-Route::get('/', [FrontController::class, 'index'])->name('frontend.index');
-
-Route::prefix('frontend')->name('frontend.')->group(function () {
-    
-    // Static pages
-    Route::get('/about', [FrontController::class, 'about'])->name('about');
-    Route::get('/events', [FrontController::class, 'events'])->name('events');
-    Route::get('/pricing', [FrontController::class, 'pricing'])->name('pricing');
-    Route::get('/blog', [FrontController::class, 'blog'])->name('blog'); // not the same as blogs
-
-    // Blogs
-    Route::prefix('blogs')->name('blogs.')->group(function () {
-        Route::get('/', [BlogController::class, 'index'])->name('index');
-        Route::get('/show/{id}', [BlogController::class, 'show'])->name('show');
-    });
-
-    // Team Members
-    Route::get('/team_members', [TeamMemberController::class, 'index'])->name('team_members.index');
-
-    // Feedback
-    Route::get('/feedback', [FeedBackController::class, 'create'])->name('feedback');
+// Base pages
+Route::controller(FrontController::class)->group(function () {
+    Route::get('/', 'index')->name('frontend.index');
+    Route::get('/about', 'about')->name('frontend.about');
+    Route::get('/events', 'events')->name('frontend.events');
+    Route::get('/pricing', 'pricing')->name('frontend.pricing');
 });
+
+// Blogs
+Route::prefix('blogs')->name('frontend.blogs.')->controller(BlogController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/show/{id}', 'show')->name('show');
+});
+
+// Team Members
+Route::get('/team_members', [TeamMemberController::class, 'index'])->name('frontend.team_members.index');
+
+// Feedback
+Route::get('/feedback', [FeedBackController::class, 'create'])->name('frontend.feedback');
