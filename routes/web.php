22b56d90265377e602c64c3a4eff15ad643be798
echobash-dtdc <anwar.ali@dtdc.com>
2025-07-25 13:26:06 +0530

    Refactor web.php and group for better readability

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,26 @@ use App\Http\Controllers\Frontend\BlogController;
 use App\Http\Controllers\Frontend\FeedBackController;
 use App\Http\Controllers\Frontend\TeamMemberController;
 
+// Home and general pages
 Route::get('/', [FrontController::class, 'index'])->name('frontend.index');
-Route::get('/about', [FrontController::class, 'about'])->name('frontend.about');
-Route::get('/blogs', [BlogController::class, 'index'])->name('frontend.blogs.index');
-Route::get('/blogs/show/{id}', [BlogController::class, 'show'])->name('frontend.blogs.show');
-Route::get('/team_members', [TeamMemberController::class, 'index'])->name('frontend.team_members.index');
-Route::get('/events', [FrontController::class, 'events'])->name('frontend.events');
-Route::get('/pricing', [FrontController::class, 'pricing'])->name('frontend.pricing');
-Route::get('/feedback', [FeedBackController::class, 'create'])->name('frontend.feedback');
-Route::get('/blog', [FrontController::class, 'blog'])->name('frontend.blog');
+
+Route::prefix('frontend')->name('frontend.')->group(function () {
+    
+    // Static pages
+    Route::get('/about', [FrontController::class, 'about'])->name('about');
+    Route::get('/events', [FrontController::class, 'events'])->name('events');
+    Route::get('/pricing', [FrontController::class, 'pricing'])->name('pricing');
+    Route::get('/blog', [FrontController::class, 'blog'])->name('blog'); // not the same as blogs
+
+    // Blogs
+    Route::prefix('blogs')->name('blogs.')->group(function () {
+        Route::get('/', [BlogController::class, 'index'])->name('index');
+        Route::get('/show/{id}', [BlogController::class, 'show'])->name('show');
+    });
+
+    // Team Members
+    Route::get('/team_members', [TeamMemberController::class, 'index'])->name('team_members.index');
+
+    // Feedback
+    Route::get('/feedback', [FeedBackController::class, 'create'])->name('feedback');
+});
